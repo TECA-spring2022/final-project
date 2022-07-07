c059aaa6dc7b0bc39af7b8d13b02bb55244c95bc
SearchAndRescueAdam <80280742+SearchAndRescueAdam@users.noreply.github.com>
2022-07-07 10:46:53 -0600

    fixed IP

--- integration test/loadbalancer-test.js
+++ integration test/loadbalancer-test.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import assert from 'assert';
 
 it ('should get Hello', async ()=>{
-    const responce  = await fetch('http://10.104.4.157 ');
+    const responce  = await fetch('http://35.243.132.37');
     const text = await responce.text();
     assert.equal(text,"Hello");
 })
