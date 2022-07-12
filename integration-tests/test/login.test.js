d242133560c1a180c3bde5f700bbb38ef6356a5c
Tuker Strand <strand.tucker@gmail.com>
2022-07-12 11:01:55 -0600

    added utils folder with login

--- integration-tests/test/login.test.js
+++ integration-tests/test/login.test.js
@@ -1,14 +1,14 @@
 import {login} from '../utils/login.js';
 
-it ("should create user", async ()=>{
-    const createUserRequest ={
-        userName: "hatersgonnahate",
-        email: "hatersgonnahate@byui.edu",
-        password:"P@ssw0rd",
-        verifyPassword:"P@ssw0rd",
-        phoneNumber: "123-345-6789"
-    }
-})
+// it ("should create user", async ()=>{
+//     const createUserRequest ={
+//         userName: "hatersgonnahate",
+//         email: "hatersgonnahate@byui.edu",
+//         password:"P@ssw0rd",
+//         verifyPassword:"P@ssw0rd",
+//         phoneNumber: "123-345-6789"
+//     }
+// })
  
 it ('Should get login token', async ()=>{
     const loginRequest = {
