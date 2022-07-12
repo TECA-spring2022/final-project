297c4c9edd6b1fb406870c41630e8b9b7273ee1d
Tuker Strand <strand.tucker@gmail.com>
2022-07-12 10:50:38 -0600

    started a create user

--- integration-tests/test/login.test.js
+++ integration-tests/test/login.test.js
@@ -1,5 +1,15 @@
-import {login} from '../utils/login.js';
+//import {login} from '../utils/login.js';
 
+it ("should create user", async ()=>{
+    const createUserRequest ={
+        userName: "hatersgonnahate",
+        email: "hatersgonnahate@byui.edu",
+        password:"P@ssw0rd",
+        verifyPassword:"P@ssw0rd",
+        phoneNumber: "123-345-6789"
+    }
+})
+ 
 it ('Should get login token', async ()=>{
     const loginRequest = {
         userName:"hatersgonnahate@byui.edu",
